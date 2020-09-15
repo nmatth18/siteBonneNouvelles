0fed1afd8bb4e84756b1d86a0fbd37cdb9f2a2d6
nmatth18 <71337287+nmatth18@users.noreply.github.com>
2020-09-15 17:26:53 +0200

    Update header.php

--- views/header.php
+++ views/header.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 	<head>
 		<meta charset="utf-8" >
-		<title>Un site de Bonnes Nouvelles</title>
+		<title>Un site de Bonnes Nouvelles on Azure</title>
 		<link rel="stylesheet" type="text/css" href="views/css/base.css" media="all" >
 		<link rel="stylesheet" type="text/css" href="views/css/modele01.css" media="screen" >
 	</head>
